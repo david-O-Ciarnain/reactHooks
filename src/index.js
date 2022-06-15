4ed6ebd2c6eb0ba5f6b9806cf5d784fba80e317e
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-06-15 16:05:08 +0200

    made header with text and fetch images from a dog image api and display in browser

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
 
-function tick(){
-    
-    const element = (
-        <div>
-            <h1>this is a clock</h1>
-            <h4>the time is {new Date().toLocaleTimeString()}</h4>
-        </div>
-    );
-    root.render(element)
-}
-setInterval(tick,1000)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(<App />)
 
 
