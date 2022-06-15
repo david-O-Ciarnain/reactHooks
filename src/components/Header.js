30060a0994ffd8dc450777bf90b8d451d7082aae
david-O-Ciarnain <david.ociarnain@utb.ecutbildning.se>
2022-06-15 16:05:45 +0200

    files that did not follow with last commit

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React,{ useEffect, useState } from "react";
+
+export default function Header (){ 
+    const [dogs,setDogs] = useState([])
+
+    useEffect(() =>{
+       fetch("https://dog.ceo/api/breeds/image/random")
+       .then(res => res.json()
+       .then(data => setDogs(data.message))) 
+        
+    },[])
+    
+    console.log(dogs)
+    
+
+return (
+    <header className="header">
+        <img
+        src={dogs} alt="dog"
+        className="header-img"
+        />
+        <h3 className="left-text"> Random dogs images and meme generator</h3>
+        <h3 className="right-text">Learning hooks in react</h3>
+
+    </header>
+)
+}
---
+++ src/components/Meme.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+
+export default function Meme(){
+
+    
+
+
+    return (
+    <>
+    <h1>testing meme components</h1>
+
+    </>
+        
+    )
+    
+}
+
+//fetch api "https://api.imgflip.com/get_memes"
